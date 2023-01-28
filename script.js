5a2c40ebe8075495b8e2b69f1d4c9d431d658c6d
Carine Polesso <111937653+carineferreiras@users.noreply.github.com>
2023-01-28 06:29:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-let imagemDoLogo;
 
-function preload(){
-    imagemDoLogo=loadImage("file:///Users/carineferreiradasilva/Documents/logo.png")
-}
-
-function setup() {
-  createCanvas(500, 400);
-}
-
-function draw() {
-  background(imagemDoLogo);
-}
